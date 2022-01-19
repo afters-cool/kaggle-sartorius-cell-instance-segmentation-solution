bfa94aeeaa22a65ce2343c804c8e4adefda41ee6
Inkyu <inkyu.sa@csiro.au>
2022-01-19 14:46:40 +1000

    update model path in test

--- IDPT_det_cots_youtube_step1.sh
+++ IDPT_det_cots_youtube_step1.sh
@@ -2,4 +2,4 @@
 python tools/det/train_cots.py configs/det/yolox_x_cots_youtube.py
 
 
-python tools/det/test_cots.py configs/det/yolox_x_cots_youtube.py work_dirs/yolox_x_livecell/epoch_30.pth --out work_dirs/yolox_x_livecell/val_preds.pkl --eval bbox
+python tools/det/test_cots.py configs/det/yolox_x_cots_youtube.py work_dirs/yolox_x_cots_youtube/epoch_30.pth --out work_dirs/yolox_x_cots_youtube/val_preds.pkl --eval bbox
